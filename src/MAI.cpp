dcdb9dbca67e8a33d6bfc3e34d53eae15fc16cfc
Vitaly <faleluhinvitaly@gmail.com>
2025-10-21 14:48:48 +0300

    blank project for reusing purposes

---
+++ src/MAI.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MAI.cpp
+// Author      : Faleluhin Vitaly
+// Version     :
+// Copyright   : no copyright
+// Description : 
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
